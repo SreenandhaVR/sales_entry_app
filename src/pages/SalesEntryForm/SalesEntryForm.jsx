0c4d971db0b3cde8ec770eeb18f5ca41bc54e8c5
Sreenandha V R <sreenandhavr161@gmail.com>
2025-08-21 19:37:46 +0530

    Styled Header

--- src/pages/SalesEntryForm/SalesEntryForm.jsx
+++ src/pages/SalesEntryForm/SalesEntryForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './home.module.scss'
 
 export default function SalesEntryForm() {
@@ -11,7 +11,15 @@ export default function SalesEntryForm() {
     });
     return (
       <div className={styles.container}>
-        <h2>Sales Entry</h2>
+        <h1 className={styles.header}>Sales Entry Form</h1>
+        <div className={styles.headerBox}>
+            <p>Voucher No: {header.vr_no}</p>
+            <p>Voucher Date: {header.vr_date}</p>
+            <p>Account Name: {header.ac_name}</p>
+            <p>Account Amount: {header.ac_amt}</p>
+            <p>Status: {header.status}</p>
+
+        </div>
       </div>
     );
   }
