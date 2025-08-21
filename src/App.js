718c2f40c1a28b627944e16c5657d2e534f00028
Sreenandha V R <sreenandhavr161@gmail.com>
2025-08-21 16:16:42 +0530

    Setting Up Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Home from './pages/Home/Home';
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import styles from './Home.module.scss'
-
+import Button from '../../components/Button/Button'
+import { Plus, Trash2, Save, Printer, Calendar } from 'lucide-react';
 const Home = () => {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <div className={styles.pageHeader}>
           <h1>Sales Entry</h1>
+          <p className={styles.subtitle}>
+            Enter your sales data and Manage Sales Vouchers.
+          </p>
+          
+          </div>
+          <div className={styles.headerActions}>
+            <Button variant="primary" icon={<Save size={20} />} >
+              Save Entry
+            </Button>
+            <Button variant="secondary" icon={<Printer size={20} />}>
+              Print
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
   )
 }
 
