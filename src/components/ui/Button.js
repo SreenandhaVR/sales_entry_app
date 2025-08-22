ee33756d09253c9a6c86940b3a2bb79fc615e2f2
Sreenandha V R <sreenandhavr161@gmail.com>
2025-08-22 08:28:05 +0530

    Create resuable Button component with variants

---
+++ src/components/ui/Button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Button = ({ 
+  children, 
+  variant = 'primary', 
+  size = 'medium', 
+  disabled = false, 
+  icon,
+  onClick,
+  type = 'button',
+  className = '',
+  ...props 
+}) => {
+  // Component implementation here
+};
+
+export default Button;
