3a5a2fb16868e95de8b6fe7ba1addab07a620fe7
Sreenandha V R <sreenandhavr161@gmail.com>
2025-08-22 08:36:26 +0530

    create Axios API base configuration with interceptors and installed axios

---
+++ src/store/services/api.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://5.189.180.8:8010';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Request interceptor
+api.interceptors.request.use(
+  (config) => {
+    console.log('Making API request:', config);
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.error('API Error:', error);
+    return Promise.reject(error);
+  }
+);
+
+export default api;
