e192a0ea869d87a29f1c2d9c79ce4b5653c338c3
Sreenandha V R <sreenandhavr161@gmail.com>
2025-08-22 08:29:17 +0530

    create reusable Input component with validation styling

---
+++ src/components/ui/Input.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Input = ({ 
+  label, 
+  error, 
+  required = false, 
+  icon, 
+  type = 'text',
+  ...props 
+}) => {
+  // Input component implementation
+};
+
+export default Input;
