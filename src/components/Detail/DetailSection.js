617de133400ce083961b3b09ee14b9892715522e
Sreenandha V R <sreenandhavr161@gmail.com>
2025-08-22 08:46:07 +0530

    add table structure to detail section

--- src/components/Detail/DetailSection.js
+++ src/components/Detail/DetailSection.js
@@ -10,14 +10,37 @@ const DetailSection = () => {
   return (
     <div className="detail-section">
       <div className="section-header">
-        <h2>Detail Information</h2>
-        <Button icon={<Plus size={18} />} onClick={addRow}>
-          Add Row
-        </Button>
-      </div>
-      {/* Table will be added here */}
+      <h2>Detail Information</h2>
+      <Button icon={<Plus size={18} />} onClick={addRow}>
+        Add Row
+      </Button>
     </div>
-  );
+    
+    <div className="table-container">
+      <div className="table-header">
+        <div>Sr. No.</div>
+        <div>Item Code</div>
+        <div>Item Name</div>
+        <div>Description</div>
+        <div>Quantity</div>
+        <div>Rate</div>
+        <div>Action</div>
+      </div>
+      
+      <div className="table-body">
+        {details.map((row, index) => (
+          <DetailRow 
+            key={index} 
+            row={row} 
+            index={index}
+            onUpdate={handleDetailChange}
+            onDelete={removeRow}
+          />
+        ))}
+      </div>
+    </div>
+  </div>
+);
 };
 
 export default DetailSection;
